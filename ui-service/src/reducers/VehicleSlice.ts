58b6adbf5158a80ca1597740e04009ed4ee5f160
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-05-15 14:48:37 +0530

    create VehicleSlice to create vehicle

--- ui-service/src/reducers/VehicleSlice.ts
+++ ui-service/src/reducers/VehicleSlice.ts
@@ -1,100 +1,45 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { Vehicle } from "../model/Vehicle";
-import Swal from "sweetalert2";
-import "../pages/style/alert.css";
+import axios from "axios";
 
 const GRAPHQL_API = "http://localhost:4000/graphql";
-export const initialState: Vehicle[] = [];
 
-// GraphQL fetcher helper
-const fetchGraphQL = async (query: string, variables?: any) => {
-  const res = await fetch(GRAPHQL_API, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ query, variables }),
-  });
-  const json = await res.json();
-  return json.data;
+export interface Vehicle {
+  id?: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  car_make: string;
+  car_model: string;
+  vin: string;
+  manufactured_date: string;
+}
+
+interface VehicleState {
+  vehicles: Vehicle[];
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: VehicleState = {
+  vehicles: [],
+  loading: false,
+  error: null,
 };
 
-// 🔄 Create
 export const createVehicle = createAsyncThunk(
-  "vehicle/createVehicle",
-  async (vehicle: Omit<Vehicle, "id">) => {
-    const mutation = `
+  "vehicle/create",
+  async (vehicleData: Vehicle) => {
+    const query = `
       mutation CreateVehicle(
-        $first_name: String!
-        $last_name: String!
-        $email: String!
-        $car_make: String!
-        $car_model: String!
-        $vin: String!
+        $first_name: String!,
+        $last_name: String!,
+        $email: String!,
+        $car_make: String!,
+        $car_model: String!,
+        $vin: String!,
         $manufactured_date: String!
       ) {
         createVehicle(
-          first_name: $first_name
-          last_name: $last_name
-          email: $email
-          car_make: $car_make
-          car_model: $car_model
-          vin: $vin
-          manufactured_date: $manufactured_date
-        ) {
-          id
-          first_name
-          last_name
-          email
-          car_make
-          car_model
-          vin
-          manufactured_date
-          age_of_vehicle
-        }
-      }
-    `;
-    const data = await fetchGraphQL(mutation, {
-      ...vehicle,
-      manufactured_date: vehicle.manufactured_date.toString(),
-    });
-
-    Swal.fire({
-      title: "✅ Success!",
-      html: '<p class="swal-text">Vehicle saved successfully.</p>',
-      icon: "success",
-      background: "white",
-      color: "black",
-      confirmButtonColor: "green",
-      timer: 2500,
-      customClass: {
-        title: "swal-title",
-        popup: "swal-popup",
-        confirmButton: "swal-button",
-      },
-    });
-
-    return data.createVehicle;
-  }
-);
-
-// 🔄 Update
-export const updateVehicle = createAsyncThunk(
-  "vehicle/updateVehicle",
-  async (vehicle: Vehicle) => {
-    const mutation = `
-      mutation UpdateVehicle(
-        $id: ID!,
-        $first_name: String,
-        $last_name: String,
-        $email: String,
-        $car_make: String,
-        $car_model: String,
-        $vin: String,
-        $manufactured_date: String
-      ) {
-        updateVehicle(
-          id: $id,
           first_name: $first_name,
           last_name: $last_name,
           email: $email,
@@ -104,92 +49,22 @@ export const updateVehicle = createAsyncThunk(
           manufactured_date: $manufactured_date
         ) {
           id
-          first_name
-          last_name
-          email
-          car_make
-          car_model
-          vin
-          manufactured_date
-          age_of_vehicle
         }
       }
     `;
 
-    const data = await fetchGraphQL(mutation, {
-      ...vehicle,
-      manufactured_date: vehicle.manufactured_date.toString(),
+    const response = await axios.post(GRAPHQL_API, {
+      query,
+      variables: vehicleData,
     });
 
-    Swal.fire({
-      title: "✅ Updated!",
-      html: '<p class="swal-text">Vehicle updated successfully.</p>',
-      icon: "success",
-      background: "white",
-      color: "black",
-      confirmButtonColor: "green",
-      timer: 2500,
-      customClass: {
-        title: "swal-title",
-        popup: "swal-popup",
-        confirmButton: "swal-button",
-      },
-    });
-
-    return data.updateVehicle;
-  }
-);
-
-// 🔄 Delete
-export const deleteVehicle = createAsyncThunk(
-  "vehicle/deleteVehicle",
-  async (id: number) => {
-    const mutation = `
-      mutation DeleteVehicle($id: ID!) {
-        deleteVehicle(id: $id)
-      }
-    `;
-    await fetchGraphQL(mutation, { id });
-
-    Swal.fire({
-      title: "✅ Deleted!",
-      html: '<p class="swal-text">Vehicle deleted successfully.</p>',
-      icon: "success",
-      background: "white",
-      color: "black",
-      confirmButtonColor: "green",
-      timer: 2500,
-      customClass: {
-        title: "swal-title",
-        popup: "swal-popup",
-        confirmButton: "swal-button",
-      },
-    });
-
-    return id;
-  }
-);
-
-// 🔄 Fetch All
-export const getVehicles = createAsyncThunk("vehicle/getVehicles", async () => {
-  const query = `
-    query GetVehicles {
-      vehicles {
-        id
-        first_name
-        last_name
-        email
-        car_make
-        car_model
-        vin
-        manufactured_date
-        age_of_vehicle
-      }
+    if (response.data.errors) {
+      throw new Error(response.data.errors[0].message);
     }
-  `;
-  const data = await fetchGraphQL(query);
-  return data.vehicles;
-});
+
+    return response.data.data.createVehicle;
+  }
+);
 
 const vehicleSlice = createSlice({
   name: "vehicle",
@@ -197,23 +72,19 @@ const vehicleSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
+      .addCase(createVehicle.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
       .addCase(createVehicle.fulfilled, (state, action) => {
-        state.push(action.payload);
+        state.loading = false;
+        state.vehicles.push(action.payload);
       })
-      .addCase(updateVehicle.fulfilled, (state, action) => {
-        const index = state.findIndex((v) => v.id === action.payload.id);
-        if (index !== -1) {
-          state[index] = action.payload;
-        }
-      })
-      .addCase(deleteVehicle.fulfilled, (state, action) => {
-        return state.filter((v) => v.id !== action.payload);
-      })
-      .addCase(getVehicles.fulfilled, (_, action) => {
-        return action.payload;
+      .addCase(createVehicle.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || "Failed to create vehicle";
       });
   },
 });
-
 
 export default vehicleSlice.reducer;
