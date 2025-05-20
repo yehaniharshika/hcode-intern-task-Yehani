4ca107458adc913e169028ce0874135b763fa63b
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-05-20 15:47:14 +0530

    changed main.tsx

--- ui-service/src/main.tsx
+++ ui-service/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./lib/apolloClient";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
