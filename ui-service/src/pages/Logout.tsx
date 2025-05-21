cd4994b929f65f57194cfdace3907e6eef9134db
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-05-21 15:53:09 +0530

    developed logout

--- ui-service/src/pages/Logout.tsx
+++ ui-service/src/pages/Logout.tsx
@@ -1,56 +1,76 @@
-// import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { useDispatch } from "react-redux";
-// import { logoutUser } from "";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Navigation } from "../components/Navigation";
+import { Container, Modal, Button } from "react-bootstrap";
 
-// import { Navigation } from "../components/Navigation.tsx";
-// import { Container, Modal, Button } from "react-bootstrap";
-// import type { AppDispatch } from "../store/store.ts";
+const Logout = () => {
+  const [show, setShow] = useState(true);
+  const navigate = useNavigate();
 
-// const Logout = () => {
-//     const [show, setShow] = useState(true); // Control modal visibility
-//     const navigate = useNavigate();
-//     const dispatch = useDispatch<AppDispatch>();
+  const handleLogout = async () => {
+    setShow(false);
+    navigate("/login");
+  };
 
-//     const handleLogout = async () => {
-//         await dispatch(logoutUser());
-//         setShow(false);
-//         navigate("/login");
-//     };
+  const handleClose = () => {
+    setShow(false);
+    navigate(-1); // Go back to previous page
+  };
 
-//     const handleClose = () => {
-//         setShow(false);
-//         navigate(-1); // Close modal and go back
-//     };
+  return (
+    <div className="flex overflow-hidden bg-emerald-200">
+      <Navigation />
+      <div className="flex-1 p-5" style={{ backgroundColor: "#c5cbe9" }}>
+        <Container fluid>
+          <Modal show={show} onHide={handleClose} centered>
+            <Modal.Header closeButton>
+              <Modal.Title
+                style={{
+                  fontFamily: "'Montserrat', serif",
+                  fontWeight: "750",
+                }}
+              >
+                Confirm Logout
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body
+              style={{
+                fontFamily: "'Montserrat', serif",
+                fontSize: "15px",
+                fontWeight: "550",
+              }}
+            >
+              Are you sure you want to log out?
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                variant="secondary"
+                onClick={handleClose}
+                style={{
+                  fontFamily: "'Montserrat', serif",
+                  fontSize: "15px",
+                  fontWeight: "550",
+                }}
+              >
+                No
+              </Button>
+              <Button
+                variant="danger"
+                onClick={handleLogout}
+                style={{
+                  fontFamily: "'Montserrat', serif",
+                  fontSize: "15px",
+                  fontWeight: "550",
+                }}
+              >
+                Yes
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
-//     return (
-//         <>
-//             <div className="flex overflow-hidden bg-emerald-200">
-//                 <Navigation />
-//                 <div className="flex-1 p-5" style={{ backgroundColor: "#cec4ff" }}>
-//                     <Container fluid>
-//                         {/* Logout Confirmation Modal */}
-//                         <Modal show={show} onHide={handleClose} centered>
-//                             <Modal.Header closeButton>
-//                                 <Modal.Title>Confirm Logout</Modal.Title>
-//                             </Modal.Header>
-//                             <Modal.Body style={{ fontFamily: "'Montserrat', serif", fontSize: "15px",fontWeight: "550" }}>
-//                                 Are you sure you want to log out?
-//                             </Modal.Body>
-//                             <Modal.Footer>
-//                                 <Button  style={{ fontFamily: "'Montserrat', serif", fontSize: "15px",fontWeight: "550" }}variant="secondary" onClick={handleClose}>
-//                                     No
-//                                 </Button>
-//                                 <Button  style={{ fontFamily: "'Montserrat', serif", fontSize: "15px",fontWeight: "550" }}variant="danger" onClick={handleLogout}>
-//                                     Yes
-//                                 </Button>
-//                             </Modal.Footer>
-//                         </Modal>
-//                     </Container>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
-
-// export default Logout;
+export default Logout;
